4d7c09e49dd4e09aa6db4042906077e3660f8171
cessscastl <u201815966@upc.edu.pe>
2022-10-29 00:00:23 -0500

    as

--- src/main/java/pe/edu/upc/postcovid/entities/Paciente.java
+++ src/main/java/pe/edu/upc/postcovid/entities/Paciente.java
@@ -28,76 +28,78 @@ public class Paciente {
     public Paciente() {
     }
 
-    public Paciente(int idPaciente, String namePaciente, String Birthday, String lastNamePaciente, String numberPhone, String numhistoria,
-                    String Diagnostico, String Version) {
+    public Paciente(int idPaciente, String namePaciente, String lastNamePaciente, String birthday, String numberPhone, String numhistoria, String diagnostico, String version) {
         this.idPaciente = idPaciente;
         this.namePaciente = namePaciente;
         this.lastNamePaciente = lastNamePaciente;
+        Birthday = birthday;
         this.numberPhone = numberPhone;
         this.numhistoria = numhistoria;
-        this.Diagnostico = Diagnostico;
-        this.Version = Version;
-        this.Birthday=Birthday;
-    }
-
-    public int getIdPaciente() {
-        return idPaciente;
+        Diagnostico = diagnostico;
+        Version = version;
     }
 
     public void setIdPaciente(int idPaciente) {
         this.idPaciente = idPaciente;
     }
 
-    public String getNamePaciente() {
-        return namePaciente;
-    }
-
     public void setNamePaciente(String namePaciente) {
         this.namePaciente = namePaciente;
-    }
-
-    public String getLastNamePaciente() {
-        return lastNamePaciente;
     }
 
     public void setLastNamePaciente(String lastNamePaciente) {
         this.lastNamePaciente = lastNamePaciente;
     }
 
-    public String getNumberPhone() {
-        return numberPhone;
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
     }
 
     public void setNumberPhone(String numberPhone) {
         this.numberPhone = numberPhone;
     }
 
-    public String Birthday() {
+    public void setNumhistoria(String numhistoria) {
+        this.numhistoria = numhistoria;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        Diagnostico = diagnostico;
+    }
+
+    public void setVersion(String version) {
+        Version = version;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public String getNamePaciente() {
+        return namePaciente;
+    }
+
+    public String getLastNamePaciente() {
+        return lastNamePaciente;
+    }
+
+    public String getBirthday() {
         return Birthday;
     }
 
-    public void Birthday(String Birthday) {
-        this.Birthday = Birthday;
+    public String getNumberPhone() {
+        return numberPhone;
     }
-    public String getnumhistoria() {
+
+    public String getNumhistoria() {
         return numhistoria;
     }
 
-    public void setnumhistoria(String numhistoria) {
-        this.numhistoria = numhistoria;
-    }
-    public String getVersion() {
-        return Version;
-    }
-
-    public void setVersion(String Version) {
-        this.Version = Version;
-    }
     public String getDiagnostico() {
         return Diagnostico;
     }
 
-    public void setDiagnostico(String Diagnostico) {
-        this.Diagnostico = Diagnostico;
+    public String getVersion() {
+        return Version;
     }
 }
