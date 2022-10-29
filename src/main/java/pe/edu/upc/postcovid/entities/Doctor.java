e6c5b6e7b8c13b9a8b7156513585514f8991af4f
Jeff0411AG <jefferson.argar@gmail.com>
2022-10-28 23:15:02 -0500

    Merge001

--- src/main/java/pe/edu/upc/postcovid/entities/Doctor.java
+++ src/main/java/pe/edu/upc/postcovid/entities/Doctor.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Doctor")
 public class Doctor {
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idDoctor;
 
