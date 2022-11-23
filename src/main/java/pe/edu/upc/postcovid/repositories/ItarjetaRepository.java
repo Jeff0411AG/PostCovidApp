296912b5b2602c620b9ca4486223dbaf2b36ece5
Jeff0411AG <jefferson.argar@gmail.com>
2022-11-23 14:46:06 -0500

    Finallll

--- src/main/java/pe/edu/upc/postcovid/repositories/ItarjetaRepository.java
+++ src/main/java/pe/edu/upc/postcovid/repositories/ItarjetaRepository.java
@@ -2,6 +2,7 @@ package pe.edu.upc.postcovid.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.postcovid.entities.Tarjeta;
 
@@ -12,5 +13,16 @@ public interface ItarjetaRepository extends JpaRepository<Tarjeta,Integer>{
 
     @Query("from Tarjeta t where upper(t.paciente.namePaciente) like %:namePatient% or lower(t.paciente.namePaciente) like %:namePatient% or t.paciente.namePaciente like %:namePaciente%")
     List<Tarjeta> buscarPorPaciente(String namePaciente);
+
+    @Query(value = "select p.name_paciente, count (t.id_tarjeta) from Tarjeta t inner join Paciente p on t.id_paciente = p.id_paciente group by p.name_paciente", nativeQuery = true)
+    List<String[]> buscarTarjetas();
+
+    List<Tarjeta> findByNumberAccount(String numero);
+
+    @Query(value = "select * From tarjeta t where t.number_account like '%4557%'", nativeQuery = true)
+
+    List<Tarjeta> buscarNumeroDominio();
+
+
 
 }
--- src/main/java/pe/edu/upc/postcovid/serviceimpls/TarjetaServiceImpl.java
+++ src/main/java/pe/edu/upc/postcovid/serviceimpls/TarjetaServiceImpl.java
@@ -1,25 +1,33 @@
 package pe.edu.upc.postcovid.serviceimpls;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.postcovid.entities.ResultadoT;
 import pe.edu.upc.postcovid.entities.Tarjeta;
 import pe.edu.upc.postcovid.repositories.ItarjetaRepository;
 import pe.edu.upc.postcovid.serviceinterfaces.ItarjetaService;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@RestController
 public class TarjetaServiceImpl implements ItarjetaService {
 
     @Autowired
     private ItarjetaRepository dS;
 
     @Override
-    public void insert(Tarjeta tarjeta) {
-        dS.save(tarjeta);
+    @Transactional
+    public boolean insert(Tarjeta tarjeta) {
+        Tarjeta objTarjeta = dS.save(tarjeta);
+        if (objTarjeta == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
-
     @Override
     public List<Tarjeta> list() {
         return dS.findAll();
@@ -42,6 +50,28 @@ public class TarjetaServiceImpl implements ItarjetaService {
     @Override
     public Optional<Tarjeta> listarid(int idTarjeta) {
         return dS.findById(idTarjeta);
+    }
+
+    @Override
+    public List<ResultadoT> buscarTarjetaCantidad() {
+        List<ResultadoT> lista= new ArrayList<>();
+        dS.buscarTarjetas().forEach(y->{
+            ResultadoT re=new ResultadoT();
+            re.setPaciente(y[0]);
+            re.setCantidad(y[1]);
+            lista.add(re);
+        });
+        return lista;
+    }
+
+    @Override
+    public List<Tarjeta> findByNumberAccount(String cuentaTarjeta) {
+        return dS.findByNumberAccount(cuentaTarjeta);
+    }
+
+    @Override
+    public List<Tarjeta> buscarNumeroDominio() {
+        return dS.buscarNumeroDominio();
     }
 
 
